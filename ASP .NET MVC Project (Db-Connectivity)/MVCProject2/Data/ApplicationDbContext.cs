f756f477075f9b358ccbcc6b8c12bc12d9fabf76
nohaill <muhammadnohail1@gmail.com>
2024-04-04 23:32:03 +0500

    MVC project 2
    Covered entity core framework migration, add data in database

---
+++ ASP .NET MVC Project (Db-Connectivity)/MVCProject2/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCProject2.Models;
+
+namespace MVCProject2.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) 
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category{CategoryId = 1, name = "Action", DisplayOrder = 1},
+                new Category{CategoryId = 2, name = "sci", DisplayOrder = 2},
+                new Category{CategoryId = 3, name = "history", DisplayOrder = 3}
+                );
+            
+        }
+    }
+}
---
+++ ASP .NET MVC Project (Db-Connectivity)/MVCProject2/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCProject2.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        
+        [Required]
+        [DisplayName("Name")]
+        public string name { get; set; }
+
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
